66cc8615e79b3efd6119f5448fb67afe77cd71c2
Camila Xavier <camilaxmedeiros@gmail.com>
2024-05-13 17:31:57 -0300

    Add the PaceIdeal project

---
+++ PaceIdeal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaceIdeal
+//
+//  Created by Camila Xavier de Medeiros on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaceIdeal/PaceIdealApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceIdealApp.swift
+//  PaceIdeal
+//
+//  Created by Camila Xavier de Medeiros on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PaceIdealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
